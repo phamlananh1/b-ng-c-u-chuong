bcab05879a9f284fbf24cda24f6d77c7dd1aca7d
phamlananh1 <phamlananh12011995@gmail.com>
2018-11-12 14:46:08 +0700

    file

---
+++ java.js
@@ -0,0 +1,12 @@
+var i,j;
+var numTable = "<table border='1' width='80%' cellspadding='0' cellpadding='5%'>"
+for (i =1; i <= 9; i++) {
+    numTable = numTable + "</tr>";
+    for (j = 2; j <= 9; j++) {
+        numTable = numTable + "<td>" + (j) + 'x' + (i) + '=' + i * j + "</td>";
+
+    }
+    numTable = numTable + "</tr>";
+}
+numTable = numTable + "</table>";
+document.write(numTable);
